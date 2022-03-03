df36dd53144768cb64207d4ddcef4c3f867443cb
PrinceDasgupta <98944034+PrinceDasgupta@users.noreply.github.com>
2022-03-03 16:21:00 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+nosex=0;
+nosey=0;
+GameStatus="";
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump=loadSound("jump.wav");
+	mario_coin=loadSound("coin.wav");
+	mario_kick=loadSound("kick.wav");
+	mario_gameover=loadSound("gameover.wav");
+	mario_die=loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas')
+	instializeInSetup(mario);
+	
+	video=createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+	
+	posenet=ml5.poseNet(video, modelLoaded);
+	posenet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+	console.log("Model has been loaded");
+}
+function gotPoses(results){
+	if(results.length>0){
+		console.log(results);
+nosex=results[0].pose.nose.x;
+nosey=results[0].pose.nose.y;
+	}
+}
+
+function draw() {
+	game()
+	console.log("nosex =" + nosex,"nosey =" + nosey );
+}
+function startGame()
+{
+ GameStatus="start";
+ document.getElementById("status").innerHTML="Game is Loaded";
+}
